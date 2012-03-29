e894b53178603fc7125c3f75d33f6afa189f9051
Dirkjan Bussink <d.bussink@gmail.com>
2012-03-29 13:13:28 +0200

    Additional setup for the Nedap AX8008
    This sets up the use GPIO ports and configures the SPI devices

--- arch/arm/mach-kirkwood/nedap-ax8008-setup.c
+++ arch/arm/mach-kirkwood/nedap-ax8008-setup.c
@@ -14,35 +14,34 @@
 #include <linux/mtd/partitions.h>
 #include <linux/ata_platform.h>
 #include <linux/mv643xx_eth.h>
+#include <linux/gpio.h>
+#include <linux/spi/flash.h>
+#include <linux/spi/spi.h>
+#include <linux/spi/orion_spi.h>
 #include <asm/mach-types.h>
 #include <asm/mach/arch.h>
 #include <mach/kirkwood.h>
+#include <mach/irqs.h>
+
 #include <plat/mvsdio.h>
 #include "common.h"
 #include "mpp.h"
 
-static struct mtd_partition nedap_ax8008_nand_parts[] = {
-	{
-		.name = "u-boot",
-		.offset = 0,
-		.size = SZ_1M
-	}, {
-		.name = "uImage",
-		.offset = MTDPART_OFS_NXTBLK,
-		.size = SZ_4M
-	}, {
-		.name = "root",
-		.offset = MTDPART_OFS_NXTBLK,
-		.size = MTDPART_SIZ_FULL
-	},
-};
+#define SPI_CS0		0
+#define SPI_CS1		7
+#define MAX3107_INT	35
+#define MV_SDA_PIN	43
+#define MV_SCL_PIN	18
+#define PFEW		4
+#define FACTRST		36
+#define DSR0		37
+#define DTR0		38
+#define RI0		41
+#define DCD0		42
+#define STS_LED		39
 
 static struct mv643xx_eth_platform_data nedap_ax8008_ge00_data = {
 	.phy_addr	= MV643XX_ETH_PHY_ADDR(8),
-};
-
-static struct mv_sata_platform_data nedap_ax8008_sata_data = {
-	.n_ports	= 2,
 };
 
 static struct mvsdio_platform_data nedap_ax8008_mvsdio_data = {
@@ -50,17 +49,44 @@ static struct mvsdio_platform_data nedap_ax8008_mvsdio_data = {
 	.gpio_card_detect	= 38,
 };
 
+static const struct flash_platform_data nedap_ax8008_spi_slave_data = {
+        .type	= "m25p128",
+};
+
+static struct spi_board_info __initdata nedap_ax8008_spi_slave_info[] = {
+        {
+                .modalias       = "m25p80",
+                .platform_data  = &nedap_ax8008_spi_slave_data,
+                .irq            = -1,
+                .max_speed_hz   = 50000000,
+                .bus_num        = 0,
+                .chip_select    = 0,
+        },
+};
+
+static struct spi_board_info nedap_ax8008_spi_uart_info[] = {
+	{
+		.modalias	= "max3107",
+		.irq		= (IRQ_KIRKWOOD_GPIO_START + MAX3107_INT),
+		.max_speed_hz   = 26000000,
+		.bus_num	= 0,
+		.chip_select	= 1,
+	},
+};
+
 static unsigned int nedap_ax8008_mpp_config[] __initdata = {
-	MPP0_NF_IO2,
-	MPP1_NF_IO3,
-	MPP2_NF_IO4,
-	MPP3_NF_IO5,
-	MPP4_NF_IO6,
-	MPP5_NF_IO7,
-	MPP18_NF_IO0,
-	MPP19_NF_IO1,
+	MPP0_GPIO,  /* SPI FLASH CS */
+        MPP4_GPIO,  /* PFEW */
+	MPP7_GPO, /*MPP7_SPI_SCn,*/ /* SPI UART CS */
+	MPP18_GPO,  /* I2C SCL */
+	MPP35_GPIO, /* SPI UART IRQ */
+        MPP36_GPIO, /* FACT RST */
 	MPP37_GPIO,
 	MPP38_GPIO,
+	MPP39_GPIO, /* STS LED */
+	MPP41_GPIO,
+	MPP42_GPIO,
+	MPP43_GPIO, /* I2C SDA */
 	0
 };
 
@@ -72,10 +98,31 @@ static void __init nedap_ax8008_init(void)
 	kirkwood_init();
 	kirkwood_mpp_conf(nedap_ax8008_mpp_config);
 
-	kirkwood_nand_init(ARRAY_AND_SIZE(nedap_ax8008_nand_parts), 25);
+	gpio_request(MAX3107_INT, "max3107_int");
+	gpio_direction_input(MAX3107_INT);
+	gpio_request(SPI_CS0, "spi_cs0");
+	gpio_direction_output(SPI_CS0, 1);
+	gpio_request(SPI_CS1, "spi_cs1");
+	gpio_direction_output(SPI_CS1, 1);
+
+	gpio_request(DTR0, "dtr0");
+	gpio_direction_input(DTR0);
+	gpio_request(DSR0, "dsr0");
+	gpio_direction_input(DSR0);
+	gpio_request(RI0, "ri0");
+	gpio_direction_input(RI0);
+	gpio_request(DCD0, "dcd0");
+	gpio_direction_input(DCD0);
+
 	kirkwood_ehci_init();
 	kirkwood_ge00_init(&nedap_ax8008_ge00_data);
-	kirkwood_sata_init(&nedap_ax8008_sata_data);
+
+	spi_register_board_info(nedap_ax8008_spi_slave_info,
+                                ARRAY_SIZE(nedap_ax8008_spi_slave_info));
+	spi_register_board_info(nedap_ax8008_spi_uart_info,
+                                ARRAY_SIZE(nedap_ax8008_spi_uart_info));
+
+	kirkwood_spi_init();
 	kirkwood_uart0_init();
 	kirkwood_sdio_init(&nedap_ax8008_mvsdio_data);
 }
